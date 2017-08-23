1a4c95136e5cd01e4a2726054319f689c8cc0505
Sam Hoffman <samdhoffman@gmail.com>
2017-08-23 14:23:00 -0400

    Add example

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+var myArray = [1, "Sam", true, null, 0];
+
+console.log("This is myArray[2]: " + myArray[2]);
+
+
+var user1 = {
+  username: "sam",
+  password: "1234"
+}
+
+var user2 = {
+  username: "bobdole",
+  password: "2345"
+}
+
+console.log("User 1 username: " + user1.username);
+console.log("User 2 username: " + user2.username);
